09636588e477b92e7bdefe9972811a990ad7e3b1
K-Sukhantharat <68940673+k-sukhantharat@users.noreply.github.com>
2021-01-18 19:55:39 +0700

    Add file source
    Mileage.txt + .py file

---
+++ 6220422081_KittisakSukhantharat_Average_Miles_per_Gallon.py
@@ -0,0 +1,37 @@
+'''
+import os
+
+with os.scandir('c:/Users/ksukh/Desktop/NIDA Python class/20200201/') as entries:
+    for entry in entries:
+        print(entry.name)
+
+'''
+def read_data2(filename):
+    data = []
+    with open(filename) as in_file:
+        for line in in_file:
+            data.append(str(line))
+    return data
+
+def read_data(filename):
+    with open(filename) as in_file:
+        lines = in_file.readlines()
+    items = map(lambda x: str(x), lines)
+    return list(items)
+
+Mileage_car = read_data2('c:/Users/ksukh/Desktop/NIDA Python class/20200201/Mileage.txt')
+car_dict = {}
+print(len(Mileage_car))
+for i in Mileage_car:
+    model = i.split(",")[0]
+    MPG = float(i.split(",")[1])
+    if model in car_dict:
+        car_dict[model]['MPG'] = car_dict[model]['MPG'] + MPG
+        car_dict[model]['number'] = car_dict[model]['number'] + 1
+    else:
+        data = {'MPG':MPG, 'number':1}
+        car_dict[model] = data
+
+for i in car_dict:
+    car_MPG_avg = car_dict[i]['MPG']/car_dict[i]['number']
+    print(f'model: {i} average MPG is {car_MPG_avg}')
